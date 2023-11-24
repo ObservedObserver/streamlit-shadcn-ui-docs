b55876790e31ad568e0a890ff8fa3bcb243c4063
observedobserver <270001151@qq.com>
2023-11-24 01:43:56 -0800

    feat: add more compoents page

---
+++ pages/Input.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import streamlit_shadcn_ui as ui
+
+st.header("Input Component")
+
+with open("docs/components/input.md", "r") as f:
+    st.markdown(f.read())
+    
+# Input Component
+input_value = ui.input(default_value="Hello, Streamlit!", type='text', placeholder="Enter text here", key="input1")
+st.write("Input Value:", input_value)
---
+++ pages/Switch.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import streamlit_shadcn_ui as ui
+
+st.header("Switch Component")
+
+with open("docs/components/switch.md", "r") as f:
+    st.markdown(f.read())
+
+# Switch Component
+switch_value = ui.switch(default_checked=True, label="Toggle Switch", key="switch1")
+st.write("Switch is On:", switch_value)
